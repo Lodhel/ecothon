a8c758249a3629a865b65030789d8c5d17535389
r.tatarinov <r.tatarinov@optimism.ru>
2024-12-01 17:54:33 +0300

    правки по загрузке данных

--- backend/app/routes/green_plant_records/green_plant_records_router.py
+++ backend/app/routes/green_plant_records/green_plant_records_router.py
@@ -141,13 +141,13 @@ class GreenPlantRouter(MainRouterMIXIN, ManagerSQLAlchemy):
                     )
                     session.add(green_plant_records)
 
+            data = self.get_data(results)
             green_plant_files = GreenPlantFiles(
-                data=results,
+                data=data,
                 file_path=temp_file_path
             )
             session.add(green_plant_files)
 
             await session.commit()
 
-        data = self.get_data(results)
         return data
